e3fc1d9b854b427248a1c09551e80db2e765c546
andrieux <florian.andrieux@sayens.fr>
2020-11-04 17:32:01 +0100

    add audio clips

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private int musicIndex = 0;
+    public AudioClip[] playlist;
+    public AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource.clip = playlist[0];
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!audioSource.isPlaying)
+        {
+            PlayNextSong();
+        }
+        
+    }
+    void PlayNextSong()
+    {
+        musicIndex = (musicIndex + 1) % playlist.Length;
+        audioSource.clip = playlist[musicIndex];
+        audioSource.Play();
+    }
+}
