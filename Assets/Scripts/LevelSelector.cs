82ac43bb2769387ef4d4cee06cd3c5ee7e975c6b
andrieux <florian.andrieux@sayens.fr>
2020-11-05 19:54:54 +0100

    allow to unlock level, if previous level is finished

--- Assets/Scripts/LevelSelector.cs
+++ Assets/Scripts/LevelSelector.cs
@@ -1,8 +1,22 @@
-﻿using UnityEngine;
+﻿using UnityEngine.UI;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using JetBrains.Annotations;
 
 public class LevelSelector : MonoBehaviour
 {
+    public Button[] levelButtons;
+    private void Start()
+    {
+        int levelReached = PlayerPrefs.GetInt("levelReached", 1);
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            if(i + 1 > levelReached)
+            {
+                levelButtons[i].interactable = false;
+            }
+        }
+    }
     public void LoadLevelPassed(string levelName)
     {
         SceneManager.LoadScene(levelName);
