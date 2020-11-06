aa8b8f8d304a09ccba7004f4940324ac56ac290e
andrieux <florian.andrieux@sayens.fr>
2020-11-06 18:17:36 +0100

    add npc with dialog ui

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DialogManager : MonoBehaviour
+{
+    public static DialogManager instance;
+    public Text nameText;
+    public Text dialogText;
+    private Queue<string> sentences;
+    public Animator animator;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Il y a plus d'une instance de DialogManager dans la scène");
+            return;
+        }
+        instance = this;
+
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialog(Dialog dialog)
+    {
+        animator.SetBool("isOpen", true);
+        nameText.text = dialog.name;
+
+        sentences.Clear();
+        foreach (string sentence in dialog.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialog();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return null;
+        }
+    }
+    private void EndDialog()
+    {
+        animator.SetBool("isOpen", false);
+
+    }
+
+}
---
+++ Assets/Scripts/DialogTrigger.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class DialogTrigger : MonoBehaviour
+{
+    private string objectEnabler = "dialog";
+    public Dialog dialog;
+    public bool isInRange;
+    void Update()
+    {
+       if(isInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            TriggerDialog();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            isInRange = true;
+            InteractionUI.instance.enabledBy.Add(objectEnabler);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            isInRange = false;
+            int objectEnablerIndex = InteractionUI.instance.enabledBy.IndexOf(objectEnabler);
+            if(objectEnablerIndex > -1)
+            {
+                InteractionUI.instance.enabledBy.RemoveAt(objectEnablerIndex); 
+            }
+        }
+        
+    }
+    private void TriggerDialog()
+    {
+        DialogManager.instance.StartDialog(dialog);
+
+    }
+}
